e3538e2f6108af0d0ae6fe7f4a3b92f199eabb3c
Timothy <t.williams.im@gmail.com>
2015-10-07 02:03:28 -0400

    added a person record, unsure how to use schema

--- src/bank_account/core.clj
+++ src/bank_account/core.clj
@@ -1,10 +1,21 @@
-(ns bank-account.core)
+(ns bank-account.core
+  (:require [schema.core :as s]))
 
 (def my-bank-account (atom 0))
+
+;(defrecord Person [fname lname bank-account-id
+
+(s/defrecord Person
+  [fname :- s/Str
+   lname :- s/Str
+   bank-account-id :- s/Int])
+
 
 (def counter (atom 0))
 
 (def bank-accounts (atom {}))
+
+(def people (atom {}))
 
 (defn withdraw [amount bank-atom]
   (swap! bank-atom #(- % amount)))
@@ -17,18 +28,4 @@
   (let [id (swap! counter inc)]
     (println "the type is" type)
     (swap! bank-accounts assoc id {:id id :type type :amount 0})))
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
-
-(add-new-account {:type "checking"})
-
-(:type (@bank-accounts 6))
-
-(add-new-account {:type "saving"})
-
-
 
